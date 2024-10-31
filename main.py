e0eabc104d20640150cac71d860b0591537a93a5
Thiago Lisboa <36263673+lisboatm@users.noreply.github.com>
2024-10-31 15:35:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Leitura da pressão desejada
+N = int(input())
+
+# Leitura da pressão lida pela bomba
+M = int(input())
+
+# Cálculo da diferença entre a pressão desejada e a pressão lida
+diferenca = N - M
+
+# Impressão do resultado
+print(diferenca)
